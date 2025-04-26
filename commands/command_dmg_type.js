5e5a4638012d9e644c3ca1ce02c9a824cfc8c328
Richard Ismer <richard.ismer@gmail.com>
2025-04-26 17:23:04 +0200

    Lint the command files

--- commands/command_dmg_type.js
+++ commands/command_dmg_type.js
@@ -6,13 +6,23 @@ export const data = new SlashCommandBuilder()
     .setDescription("dit si le perso est ap ou ad")
     .addStringOption(option => option.setName("champ").setDescription("le perso à tester").setRequired(true));
 
-export async function execute(interaction) {
+export async function execute(interaction) 
+{
+    // Get the champion from the interaction
     const champ = interaction.options.getString("champ");
 
+    // Get the champion data from the champion list
     const champData = champions.champions[champ];
-    if (champData == null) {
+
+    // Check if the champion is in the list
+    if (champData == null) 
+    {
+        // The champion was not found, display an error message
         interaction.reply("Champ not found");
-    } else {
+    } 
+    else 
+    {
+        // The champion was found, display the damage type
         interaction.reply(champ + " does " + champData.damage_type + " damage");
     }
 }
--- commands/command_otp.js
+++ commands/command_otp.js
@@ -31,16 +31,25 @@ export const data = new SlashCommandBuilder()
     .addStringOption(option => option.setName("summoner").setDescription("l'utilisateur à tester").setRequired(true))
     .addStringOption(option => option.setName("region").setDescription("la région de l'utilisateur").setRequired(false))
 
-export async function execute(interaction) {
-    
-
+export async function execute(interaction) 
+{    
     // get region from interaction
     var region = null
-    if (interaction.options.getString("region") == null) {
+
+    // Chech if region is set
+    if (interaction.options.getString("region") == null) 
+    {
+        // The region is not set, use the default region
         region = regions.EUW
-    } else if (interaction.options.getString("region") in regions) {
+    } 
+    else if (interaction.options.getString("region") in regions) 
+    {
+        // The region is set and in the list of regions
         region = regions[interaction.options.getString("region")]
-    } else {
+    } 
+    else 
+    {
+        // The region is not set and not in the list of regions
         interaction.reply("Region not found: " + interaction.options.getString("region"))
         return
     }
@@ -51,41 +60,62 @@ export async function execute(interaction) {
     var resp = null
 
     // get summoner from riot api
-    try {
+    try 
+    {
+        // Try to get the summoner by name
         summoner = await api.Summoner.getByName(name, region)
-    } catch (e) {
+    } 
+    catch (e) 
+    {
+        // The summoner was not found, display an error message
         console.log(e)
         interaction.reply("Summoner not found: " + e)
         return
     }
 
-    // get summoner masteries from riot api
-    try {
+    // Get summoner masteries from riot api
+    try 
+    {
+        // Try to get the summoner masteries
         resp = await api.Champion.masteryBySummoner(summoner.response.id, region)
-    } catch (e) {
+    } 
+    catch (e) 
+    {
+        // The summoner masteries were not found, display an error message
         console.log(e)
         interaction.reply("Masteries not found: " + e)
         return
     }
+
+    // Get the summoner masteries from the response
     const masteries = resp.response
 
+    // Get the 3 highest masteries
     let first = masteries[0]
     let second = masteries[1]
     let third = masteries[2]
 
     // check if summoner is low level
-    if (first.championPoints < 50000) {
+    if (first.championPoints < 50000) 
+    {
         interaction.reply(name + " : Just another player")
     }
-    // check if summoner is otp
-    if (first.championPoints > second.championPoints * 5) {
+
+    // Check if summoner is otp
+    if (first.championPoints > second.championPoints * 5) 
+    {
+        // The summoner is otp, display the champion name and points
         const champion = await api.DataDragon.getChampion(first.championId)
         interaction.reply(name + " : OTP INCOMING ("+ champion.id + " : " + first.championPoints + " points)")
-    } else {
+    }
+    else 
+    {
+        // The summoner is not otp, display the champion names and points
         const champ1 = await api.DataDragon.getChampion(first.championId)
         const champ2 = await api.DataDragon.getChampion(second.championId)
         const champ3 = await api.DataDragon.getChampion(third.championId)
         
+        // Craft the response message   
         interaction.reply(name + " : Just another player " + 
         "\n\tmain: \t" + champ1.id + " (" + first.championPoints + ")" +
         "\n\tsecond: " + champ2.id + " (" + second.championPoints + ")" +
